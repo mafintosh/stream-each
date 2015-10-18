e21ef811a1ebd9f629cb7899d7113177171d798a
Mathias Buus <mathiasbuus@gmail.com>
2015-10-18 22:45:16 +0200

    tidy style and add standard

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ module.exports = each
 
 function each (stream, fn) {
   var want = false
-
-  stream.on('readable', function () {
-    if (want) loop(null)
-  })
+  stream.on('readable', onreadable)
   loop(null)
-
   return stream
+
+  function onreadable () {
+    if (want) loop(null)
+  }
 
   function loop (err) {
     if (err) return stream.destroy(err)
