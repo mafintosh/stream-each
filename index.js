ca13326c6bdb8f9bff7b29aba5bc6a8ce767aaf9
Mathias Buus <mathiasbuus@gmail.com>
2016-01-26 01:57:50 +0100

    fix callback being called too early

--- index.js
+++ index.js
@@ -4,19 +4,33 @@ module.exports = each
 
 function each (stream, fn, cb) {
   var waiting = false
+  var error = null
+  var ended = false
+
   stream.on('readable', onreadable)
   read(null)
-  if (cb) eos(stream, {readable: true, writable: false}, cb)
+  if (cb) eos(stream, {readable: true, writable: false}, done)
   return stream
+
+  function done (err) {
+    error = err
+    ended = true
+    if (waiting) return cb(error)
+  }
 
   function onreadable () {
     if (waiting) read(null)
   }
 
   function read (err) {
-    if (err) return stream.destroy(err)
+    if (err) {
+      waiting = true
+      stream.destroy(err)
+      return
+    }
     var data = stream.read()
     waiting = !data
+    if (waiting && ended) return cb(error)
     if (data) fn(data, read)
   }
 }
